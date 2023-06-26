09bcd8ca59d105d0e43ff84bc6a2820aac5b1551
nitsuahlabs@gmail.com <nitsuahlabs@gmail.com>
2023-06-26 16:27:40 -0400

    wagmi + nextjs + etherscan setup

---
+++ src/wagmi.ts
@@ -0,0 +1,45 @@
+import { configureChains, createConfig } from 'wagmi'
+import { goerli, mainnet, polygonMumbai, baseGoerli } from 'wagmi/chains'
+import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet'
+import { InjectedConnector } from 'wagmi/connectors/injected'
+import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
+import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
+
+import { publicProvider } from 'wagmi/providers/public'
+
+const walletConnectProjectId = '732797c00bb7ff1ca10685d9b9415cb6'
+
+const { chains, publicClient, webSocketPublicClient } = configureChains(
+  [mainnet, ...(process.env.NODE_ENV === 'development' ? [goerli] : [])],
+  [
+    publicProvider(),
+  ],
+)
+
+export const config = createConfig({
+  autoConnect: true,
+  connectors: [
+    new MetaMaskConnector({ chains }),
+    new CoinbaseWalletConnector({
+      chains,
+      options: {
+        appName: 'wagmi',
+      },
+    }),
+    new WalletConnectConnector({
+      chains,
+      options: {
+        projectId: walletConnectProjectId,
+      },
+    }),
+    new InjectedConnector({
+      chains,
+      options: {
+        name: 'Injected',
+        shimDisconnect: true,
+      },
+    }),
+  ],
+  publicClient,
+  webSocketPublicClient,
+})
