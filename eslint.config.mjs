692cfeafcae17a2116e15aa00b2c7eff65455cbb
nitsuahlabs@gmail.com <nitsuahlabs@gmail.com>
2025-05-03 17:59:10 -0400

    chore: update ESLint configuration and dependencies
    - Removed .eslintrc.json in favor of eslint.config.mjs.
    - Updated eslint.config.mjs to include ignore patterns and React version settings.
    - Upgraded ESLint and related packages to the latest versions.
    - Adjusted TypeScript version in package.json and package-lock.json.
    - Cleaned up package-lock.json by removing outdated dependencies.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,10 +4,24 @@ import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
-
 export default defineConfig([
+  {
+    ignores: [
+      "node_modules",
+      ".next",
+      "out",
+      "dist"
+    ]
+  },
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  {
+    settings: {
+      react: {
+        version: "18.2"
+      }
+    }
+  }
 ]);
