eefb11a0b757c20b2a55c5a2939821cbbb81f235
nitsuahlabs@gmail.com <nitsuahlabs@gmail.com>
2025-10-27 00:14:37 -0400

    fix: remove unused imports and clean up code in multiple lab components

--- src/app/labs/dao/page.tsx
+++ src/app/labs/dao/page.tsx
@@ -1,18 +1,12 @@
 // DAO - src/app/labs/DAO/page.tsx //TODO
 "use client";
-import React, { useEffect, useState } from "react";
-import Image from "next/image";
-import { Button, TextField, Grid, Box } from "@mui/material";
 
 // LAB STYLES
-import "../../_components/_styles/labs.css";
-import LabNav from "../../_components/_labs/LabNav";
 import LabFooter from "../../_components/_labs/LabFooter";
+import LabNav from "../../_components/_labs/LabNav";
+import "../../_components/_styles/labs.css";
 
-// LAB ASSETS
-import mumbai from "../../_components/_web3/_assets/mumbai.png";
-import polygonLogo from "../../_components/_web3/_assets/polygonlogo.png";
-import ethLogo from "../../_components/_web3/_assets/ethlogo.png";
+// LAB ASSETS (unused logos removed until needed)
 
 const DaoSite = () => {
   return (
