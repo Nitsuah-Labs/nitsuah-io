3a3529fc741c4e4ea4b08c61156ce4aead88309a
nitsuahlabs@gmail.com <nitsuahlabs@gmail.com>
2025-10-27 22:13:48 -0400

    chore: remove duplicate root configs; import connectors from @wagmi/connectors

--- src/app/_components/_spline/spline-home.tsx
+++ src/app/_components/_spline/spline-home.tsx
@@ -1,11 +1,18 @@
 // Spline Home Navigation
 "use client";
-import Spline from "@splinetool/react-spline";
+import dynamic from "next/dynamic";
 
 // Set the scene URL
 const SPLINE_SCENE = `https://prod.spline.design/I53VLJ6OCdGVN8lr/scene.splinecode`;
 
+const Spline = dynamic(() => import("@splinetool/react-spline"), {
+  ssr: false,
+});
+
 export function SplineScene() {
+  // Render the dynamic Spline component client-side only
+  // @ts-ignore - dynamic import provides the component at runtime
   return <Spline scene={SPLINE_SCENE} />;
 }
+
 export default SplineScene;
