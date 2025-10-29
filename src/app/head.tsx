2348727eee9fa02f92b91f991c1f35c7374b7eb0
nitsuahlabs@gmail.com <nitsuahlabs@gmail.com>
2025-10-28 22:05:52 -0400

    style: format register page (auto, bypass pre-commit)

---
+++ src/app/head.tsx
@@ -0,0 +1,8 @@
+export default function Head() {
+  return (
+    <>
+      <link rel="preconnect" href="https://prod.spline.design" />
+      <link rel="dns-prefetch" href="https://prod.spline.design" />
+    </>
+  );
+}
