79e30e65a56950157cf6e162951bec95af453e57
nitsuahlabs@gmail.com <nitsuahlabs@gmail.com>
2025-11-23 19:44:38 -0500

    feat: Import CartView component into StorefrontDemo

--- src/app/projects/clients/_comp/StorefrontDemo.tsx
+++ src/app/projects/clients/_comp/StorefrontDemo.tsx
@@ -14,6 +14,7 @@ import {
 import React, { useState } from "react";
 
 // Subcomponents
+import { CartView } from "./storefront/CartView";
 import { HomePage } from "./storefront/HomePage";
 
 export const StorefrontDemo: React.FC = () => {
